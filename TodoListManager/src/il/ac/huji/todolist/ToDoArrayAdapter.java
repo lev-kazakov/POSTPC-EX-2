36362f53a978262c55cd03a1304c9b50feb229e3
lev-kazakov <lev.kazakov@gmail.com>
2014-03-01 21:09:59 +0200

    work's done

---
+++ TodoListManager/src/il/ac/huji/todolist/ToDoArrayAdapter.java
@@ -0,0 +1,113 @@
+package il.ac.huji.todolist;
+
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class ToDoArrayAdapter<S> extends ArrayAdapter<String> {
+	
+	private static final int TYPE_FIRST = 0;
+    private static final int TYPE_SECOND = 1;
+    private static final int TYPE_MAX_COUNT = TYPE_SECOND + 1;
+	
+	private List<String> todoList;
+	private LayoutInflater inflater;
+	private Context mContext;
+	
+	public ToDoArrayAdapter(Context context, int resource, List<String> list) {
+		super(context, resource, list);
+		mContext = context;
+		todoList = list;
+	}
+
+	@Override
+    public int getCount() {
+        return todoList.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return todoList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+	
+    @Override
+    public void add(String item) {
+    	todoList.add(item);
+        notifyDataSetChanged();
+    }
+    
+    @Override
+    public int getViewTypeCount() {
+    	return TYPE_MAX_COUNT;
+    }
+    
+    public int getItemViewType(int position) {
+        return (position % 2 == 0) ? TYPE_FIRST : TYPE_SECOND;
+    }
+    
+    public static class ViewHolder {
+        @SuppressWarnings("unchecked")
+        public static <T extends View> T get(View view, int id) {
+        	Log.v("Adapter: ", "-	ENTER ViewHolder");
+            SparseArray<View> viewHolder = (SparseArray<View>) view.getTag();
+            if (viewHolder == null) {
+            	Log.v("Adapter: ", "-	Setting tag");
+                viewHolder = new SparseArray<View>();
+                view.setTag(viewHolder);
+            }
+            
+            View childView = viewHolder.get(id);
+            if (childView == null) {
+            	Log.v("Adapter: ", "-	adding child to array");
+                childView = view.findViewById(id);
+                viewHolder.put(id, childView);
+            }
+            Log.v("Adapter: ", "-	EXIT ViewHolder");
+            return (T) childView;
+        }
+    }
+    
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		Log.d("Adapter: ", "ENTER getView " + position + " " + (convertView != null));
+		
+		View view = convertView;
+		
+		if (view == null) {
+			inflater = LayoutInflater.from(getContext());
+			view = inflater.inflate(R.layout.row, parent, false);
+		}
+		
+		// get todoItem
+		TextView todoItem = ViewHolder.get(view, R.id.todoItem);
+		// set color
+		int type = getItemViewType(position);
+		switch (type) {
+		case TYPE_FIRST:
+			todoItem.setTextColor(Color.RED); // better mContext.getResources().getColor(R.color.firstItem)?
+			break;
+		case TYPE_SECOND:
+			todoItem.setTextColor(Color.BLUE); // better mContext.getResources().getColor(R.color.secondItem)?
+			break;
+		}
+		// set text
+		todoItem.setText(getItem(position));
+		
+		Log.v("Adapter: ", "EXIT getView " + Integer.toString(position));
+		
+		return view;
+	}
+}
--- TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
+++ TodoListManager/src/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,23 +1,86 @@
 package il.ac.huji.todolist;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class TodoListManagerActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todo_list_manager);
-    }
+	private ArrayAdapter<String> todosAdapter;
+	private ArrayList<String> todos;
+	final Context context = this;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_todo_list_manager);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.todo_list_manager, menu);
-        return true;
-    }
-    
+		ListView lstTodoItems = (ListView) findViewById(R.id.lstTodoItems);
+		lstTodoItems.setEmptyView(findViewById(android.R.id.empty));
+		
+		todos = new ArrayList<String>(); // getResources().getStringArray(R.array.strArrTodos);
+
+		todosAdapter = new ToDoArrayAdapter<String>(getApplicationContext(), R.layout.row, todos);
+
+		lstTodoItems.setAdapter(todosAdapter);
+		
+		// delete task interface
+		lstTodoItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(android.widget.AdapterView<?> a, View view, final int position, long id) {
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+				TextView title = (TextView) View.inflate(context, R.layout.alert_title, null);
+				title.setText(((TextView) view.findViewById(R.id.todoItem)).getText());
+				
+				alertDialogBuilder
+				.setCustomTitle(title)
+				.setPositiveButton(R.string.alertDelete, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						todos.remove(position);
+						todosAdapter.notifyDataSetChanged();
+					}
+				  });
+ 
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				alertDialog.show();
+				
+				return true;
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.todo_list_manager, menu);
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menuItemAdd:
+			final EditText edtNewItem = (EditText) findViewById(R.id.edtNewItem);
+			if (edtNewItem.getText().length() == 0) {
+				return true;
+			}
+			todos.add(edtNewItem.getText().toString());
+			todosAdapter.notifyDataSetChanged();
+			edtNewItem.setText("");
+			break;
+		}
+		return true;
+	}
+
 }
